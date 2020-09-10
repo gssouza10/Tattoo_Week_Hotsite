9d662655cd381ff6cb69f3c09dc82211aa061d90
Bruno Godinho <gm.brunoo@gmail.com>
2020-09-10 18:57:28 -0300

    nova função para o active do menu principal

--- scripts/script.js
+++ scripts/script.js
@@ -25,3 +25,15 @@ function myFunction3() {
   document.body.style.backgroundImage = "url('images/background_tickets.jpg')";
   
 }
+
+$('#navbar ul li').click(function(){
+  $('#navbar ul li').removeClass('activo');    
+  $(this).addClass('activo');
+  
+});
+
+$('.navigation ol li').click(function(){
+  $('.navigation ol li').removeClass('activo');    
+  $(this).addClass('activo');
+  
+});
